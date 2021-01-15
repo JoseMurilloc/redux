7ca055282e46d1e24da99cb3576ccf3a6157c12a
José Murillo <jooseemurillo@gmail.com>
2021-01-15 08:55:33 -0300

    👾 CatallogItem

---
+++ src/components/CatalogItem.tsx
@@ -0,0 +1,31 @@
+import React, { useCallback } from 'react';
+import { useDispatch } from 'react-redux';
+import { addProductToCart } from '../store/modules/cart/actions';
+import { IProduct } from '../store/modules/cart/types';
+
+interface CatalogItemProps {
+  product: IProduct;
+}
+const CatalogItem: React.FC<CatalogItemProps> = ({ product }) => {
+  const dispatch = useDispatch();
+
+  const handleAddToCart = useCallback(() => {
+    dispatch(addProductToCart(product))
+  },[dispatch, product])
+
+  return (
+    <article key={product.id}>
+      <strong>{product.title}</strong> {" - "}
+      <strong>{product.price}</strong> {" "}
+
+      <button
+        type="button"
+        onClick={handleAddToCart}
+      >
+        Comprar
+      </button>
+    </article>
+  )
+}
+
+export default CatalogItem;
--- src/components/Catalogy.tsx
+++ src/components/Catalogy.tsx
@@ -1,11 +1,9 @@
-import { useCallback, useEffect, useState } from "react";
-import { useDispatch } from "react-redux";
+import { useEffect, useState } from "react";
 import api from "../services/api";
-import { addProductToCart } from "../store/modules/cart/actions";
 import { IProduct } from "../store/modules/cart/types";
+import CatalogItem from "./CatalogItem";
 
 const Catalogy: React.FC = () => {
-  const dispatch = useDispatch();
   const [catalog, setCatalog] = useState<IProduct[]>([]);
 
   useEffect(() => {
@@ -14,25 +12,13 @@ const Catalogy: React.FC = () => {
     });
   }, [])
 
-  const handleAddToCart = useCallback((product: IProduct) => {
-    dispatch(addProductToCart(product))
-  },[dispatch])
+
 
   return (
     <main>
       <h1>Catalog</h1>
       {catalog.map(product => (
-        <article key={product.id}>
-          <strong>{product.title}</strong> {" - "}
-          <strong>{product.price}</strong> {" "}
-
-          <button
-           type="button"
-           onClick={() => handleAddToCart(product)}
-          >
-            Comprar
-          </button>
-        </article>
+        <CatalogItem key={product.id} product={product} />        
       ))}
     </main>
   );
