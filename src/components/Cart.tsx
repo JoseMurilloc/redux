413f5ead58f8081335f3c9fda10181ca83382f4c
José Murillo <jooseemurillo@gmail.com>
2021-01-16 15:26:12 -0300

    Minimal config redux saga

--- src/components/Cart.tsx
+++ src/components/Cart.tsx
@@ -7,7 +7,6 @@ const Cart: React.FC = () => {
   const cart = useSelector<IState, ICartItem[]>(
     state => state.cart.items
   )
-  console.log(cart)
   
   return (
     <table>
--- src/store/index.ts
+++ src/store/index.ts
@@ -1,15 +1,27 @@
+import createSagaMiddleware from 'redux-saga';
+
 import { ICartState } from './modules/cart/types';
-import { createStore } from "redux";
-import rootReducer from "./rootReducer";
+import { applyMiddleware, createStore } from "redux";
 import { composeWithDevTools } from 'redux-devtools-extension';
+
+import rootReducer from "./rootReducer";
+import rootSaga from './rootSaga';
 
 export interface IState {
   cart: ICartState;
 }
 
+const sagaMiddleware = createSagaMiddleware();
+
+const middlewares = [sagaMiddleware]
+
 const store = createStore(
   rootReducer,
-  composeWithDevTools()
+  composeWithDevTools(
+    applyMiddleware(...middlewares)
+  )
 );
+
+sagaMiddleware.run(rootSaga);
 
 export default store;
